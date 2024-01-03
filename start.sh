b10069e95bb7682f0e7b8df2cf881ac01823deb5
Hoppenbrouwers Techniek <153098893+HoppenbrouwersTechniek@users.noreply.github.com>
2024-01-03 14:23:28 +0100

    version 2 created

--- start.sh
+++ start.sh
@@ -1,82 +1,103 @@
 #!/usr/bin/bash
-
-echo "  _    _                              _                                            ";
-echo " | |  | |                            | |                                           ";
-echo " | |__| | ___  _ __  _ __   ___ _ __ | |__  _ __ ___  _   ___      _____ _ __ ___  ";
-echo " |  __  |/ _ \| '_ \| '_ \ / _ \ '_ \| '_ \| '__/ _ \| | | \ \ /\ / / _ \ '__/ __| ";
-echo " | |  | | (_) | |_) | |_) |  __/ | | | |_) | | | (_) | |_| |\ V  V /  __/ |  \__ \ ";
-echo " |_|  |_|\___/| .__/| .__/ \___|_| |_|_.__/|_|  \___/ \__,_| \_/\_/ \___|_|  |___/ ";
-echo "              | |   | |                                                            ";
-echo "  _       _   |_|___|_|                           _               _              _ ";
-echo " (_)     | |    / ____|                          | |             | |            | |";
-echo "  _  ___ | |_  | |     ___  _ __  _ __   ___  ___| |_ ___  _ __  | |_ ___   ___ | |";
-echo " | |/ _ \| __| | |    / _ \| '_ \| '_ \ / _ \/ __| __/ _ \| '__| | __/ _ \ / _ \| |";
-echo " | | (_) | |_  | |___| (_) | | | | | | |  __/ (__| || (_) | |    | || (_) | (_) | |";
-echo " |_|\___/ \__|  \_____\___/|_| |_|_| |_|\___|\___|\__\___/|_|     \__\___/ \___/|_|";
-echo "                                                                                ";
-echo "                                                                                   ";
-echo "V1.0";
-echo "";
-echo "";
-echo "";
-echo "";
-echo -e "Enter the Connection String for The iotHub";
-read connectionString
-
-echo "starting installing tools";
-
-echo "*- installing IoT Edge";
-
-wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+ 
+version="2.0"
+ 
+banner() {
+    echo "";
+    echo " | |  | |                            | |                                           ";
+    echo " | |__| | ___  _ __  _ __   ___ _ __ | |__  _ __ ___  _   ___      _____ _ __ ___  ";
+    echo " |  __  |/ _ \| '_ \| '_ \ / _ \ '_ \| '_ \| '__/ _ \| | | \ \ /\ / / _ \ '__/ __| ";
+    echo " | |  | | (_) | |_) | |_) |  __/ | | | |_) | | | (_) | |_| |\ V  V /  __/ |  \__ \ ";
+    echo " |_|  |_|\___/| .__/| .__/ \___|_| |_|_.__/|_|  \___/ \__,_| \_/\_/ \___|_|  |___/ ";
+    echo "              | |   | |                                                            ";
+    echo "  _       _   |_|___|_|                           _               _              _ ";
+    echo " (_)     | |    / ____|                          | |             | |            | |";
+    echo "  _  ___ | |_  | |     ___  _ __  _ __   ___  ___| |_ ___  _ __  | |_ ___   ___ | |";
+    echo " | |/ _ \| __| | |    / _ \| '_ \| '_ \ / _ \/ __| __/ _ \| '__| | __/ _ \ / _ \| |";
+    echo " | | (_) | |_  | |___| (_) | | | | | | |  __/ (__| || (_) | |    | || (_) | (_) | |";
+    echo " |_|\___/ \__|  \_____\___/|_| |_|_| |_|\___|\___|\__\___/|_|     \__\___/ \___/|_|";
+    echo -e "\n";
+    echo "V${version}";
+}
+ 
+sucessBanner() {
+    echo "Setup done!"
+    echo " __ __   ____  __ __    ___      _____  __ __  ____       __ ";
+    echo "|  |  | /    ||  |  |  /  _]    |     ||  |  ||    \     |  |";
+    echo "|  |  ||  o  ||  |  | /  [_     |   __||  |  ||  _  |    |  |";
+    echo "|  _  ||     ||  |  ||    _]    |  |_  |  |  ||  |  |    |__|";
+    echo "|  |  ||  _  ||  :  ||   [_     |   _] |  :  ||  |  |     __ ";
+    echo "|  |  ||  |  | \   / |     |    |  |   |     ||  |  |    |  |";
+    echo "|__|__||__|__|  \_/  |_____|    |__|    \__,_||__|__|    |__|";
+    echo "                                                             ";
+}
+ 
+checkInstalled() {
+    if ! which "$1" &> /dev/null; then
+        echo -e "\033[37;41;1;4m$1 not available, installation failed!\033[0m"
+        exit 1
+    else
+        echo -e "\033[37;42m$1 is available.\033[0m"
+    fi
+}
+ 
+getConnectionString() {
+    read connectionString
+    if ! -z "$connectionString"; then
+        echo "The connection string may not be empty, try again."
+        getConnectionString
+    elif ! echo "$connectionString" | grep "DeviceId=" &> /dev/null; then
+        echo "The connection is missing the device identifier, try again."
+        getConnectionString
+    fi
+}
+ 
+if ! lsb_release --id | grep Ubuntu &> /dev/null; then
+    echo "This script is intended for Ubuntu, not $(lsb_release --id --short)!"
+    exit 2
+fi
+ 
+banner
+ 
+checkInstalled curl
+ 
+echo "Enter the Connection String for Azure IoT-Hub";
+ 
+getConnectionString
+ 
+echo -e "\033[30;47;1m(1/3) Installing IoT Edge...\033[0m";
+ 
+ubuntu_version=$(lsb_release --release --short)
+package_sources_url="https://packages.microsoft.com/config/ubuntu/${version}/packages-microsoft-prod.deb"
+if ! curl -ssl "$package_sources_url" -o packages-microsoft-prod.deb; then
+    echo "Failed to get package sources from '$package_sources_url'."
+    exit 3
+fi
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
-
-echo "________________________________________________________________________________"
-echo "________________________________________________________________________________"
-echo "________________________________________________________________________________"
-
-echo "*- installing container engine"
-sudo apt-get update
-sudo apt-get -y install moby-engine
-
+ 
+echo -e "\033[30;47;1m(2/3) Installing container engine...\033[0m"
+sudo apt update
+sudo apt -y install moby-engine
+checkInstalled docker
+ 
+duration=0
+echo "Starting Docker"
 sudo systemctl restart docker
-
-echo "sleep for 1 minute, waiting for docker to restart"
-
-duration=60
-
-while [ "$duration" -gt 0 ]; do
-  echo -ne "$duration\033[0K\r"  # \033[0K clears the line  sleep 1
+while ! systemctl is-active --quiet docker;
+do
+  echo -ne "Awaiting Docker (${duration}s)...\033[0K\r"
   sleep 1
-  ((duration--))
+  ((duration++))
 done
-
-echo "________________________________________________________________________________"
-echo "________________________________________________________________________________"
-echo "________________________________________________________________________________"
-
-
-echo "*- installing IoT Edge runtime"
-
-sudo apt-get -y install aziot-edge
-
-echo "________________________________________________________________________________"
-echo "________________________________________________________________________________"
-echo "________________________________________________________________________________"
-
-sudo iotedge config mp --connection-string $connectionString
-
+echo "Docker is active"
+ 
+echo -e "\033[30;47;1m(3/3) Installing IoT Edge runtime...\033[0m"
+ 
+sudo apt -y install aziot-edge
+checkInstalled iotedge
+ 
+sudo iotedge config mp --connection-string "$connectionString"
 sudo iotedge config apply
-
-echo "Setup done!"
-
-echo " __ __   ____  __ __    ___      _____  __ __  ____       __ ";
-echo "|  |  | /    ||  |  |  /  _]    |     ||  |  ||    \     |  |";
-echo "|  |  ||  o  ||  |  | /  [_     |   __||  |  ||  _  |    |  |";
-echo "|  _  ||     ||  |  ||    _]    |  |_  |  |  ||  |  |    |__|";
-echo "|  |  ||  _  ||  :  ||   [_     |   _] |  :  ||  |  |     __ ";
-echo "|  |  ||  |  | \   / |     |    |  |   |     ||  |  |    |  |";
-echo "|__|__||__|__|  \_/  |_____|    |__|    \__,_||__|__|    |__|";
-echo "                                                             ";
-
-
+ 
+sucessBanner
